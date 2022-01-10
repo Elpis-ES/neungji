69110af24690028c8c6a55be6425cea4a1f3c4cb
Elpis <elpis@Elpisui-MacBookAir.local>
2022-01-11 03:26:16 +0900

    11066: Apply Knuth optimization
    A very good explaination of this optimization is in
    https://koosaga.com/243

--- 11066.cpp
+++ 11066.cpp
@@ -3,32 +3,38 @@
 using namespace std;
 
 int dp_arr[502][502];
+int k_arr[502][502];
 int sum_arr[502];
 
 int main(){
-    int t, k, n, num, tmp, cur_min;
+    int t, k, n, num, tmp, i, j, cur_min, opt_val;
     scanf(" %d", &t);
-    for(int i = 0; i < t; i++){
+    for(int p = 0; p < t; p++){
         scanf(" %d", &k);
         memset(dp_arr, 0, sizeof(int)*502*502);
+        memset(k_arr, 0, sizeof(int)*502*502);
         memset(sum_arr, 0, sizeof(int)*502);
-        for(int j = 1; j <= k; j++){
+        for(int q = 1; q <= k; q++){
             scanf(" %d", &num);
-            sum_arr[j] = sum_arr[j-1] + num;
-            dp_arr[j][j] = 0;
+            sum_arr[q] = sum_arr[q-1] + num;
+            k_arr[q][q] = q;
         }
-        for(int p = k - 1; p >=1; p--){
-            for(int r = p + 1; r <= k; r++){
+        for(int m = 1; m <= k-1; m++){
+            for(i = 1; i <= k - m; i++){
+                j = i + m;
                 cur_min = INT_MAX;
-                for(int q = p; q < r; q++){
-                    tmp = dp_arr[p][q] + dp_arr[q+1][r] + sum_arr[r] - sum_arr[p-1];
+                for(int opt = k_arr[i][j-1]; opt <= k_arr[i+1][j]; opt++){
+                    tmp = dp_arr[i][opt] + dp_arr[opt+1][j] + sum_arr[j] - sum_arr[i - 1];
                     if(tmp < cur_min){
                         cur_min = tmp;
+                        opt_val = opt;
                     }
                 }
-                dp_arr[p][r] = cur_min;
+                dp_arr[i][j] = cur_min;
+                k_arr[i][j] = opt_val;
             }
         }
+
         printf("%d\n", dp_arr[1][k]);
 
     }
