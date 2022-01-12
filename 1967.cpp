cf60864cad1f94f9a8400ca296b716f12532f059
Elpis <elpis@Elpisui-MacBookAir.local>
2022-01-13 00:16:43 +0900

    1967: Be careful of memory limits

--- 1967.cpp
+++ 1967.cpp
@@ -1,16 +1,48 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-int tree[10002][10002];
+vector<pair<int, int> >v[10001];
+bool visited[10001];
+int n;
+int max_node = 0;
+int max_len = 0;
+
+void dfs(int root, int cur_len){
+    if(visited[root]){
+        return;
+    }
+    visited[root] = true;
+    if(cur_len > max_len){
+        max_node = root;
+        max_len = cur_len;
+    }
+
+    int idx;
+    int len;
+    for(auto it = v[root].begin(); it!= v[root].end(); it++){
+        idx = it->first;
+        len = it->second;
+        if(!visited[idx]){
+            dfs(idx, cur_len + len);
+        }
+    }
+    return;
+}
 
 int main(void){
     int f, t, w;
-    int n, i;
+    int i;
     scanf(" %d", &n);
-    for(i = 0; i < n; i++){
+    for(i = 0; i < n - 1; i++){
         scanf (" %d %d %d", &f, &t, &w);
-        tree[f][t] = w;
-        tree[t][f] = w;
+        v[f].push_back(make_pair(t, w));
+        v[t].push_back(make_pair(f, w));
     }
+    dfs(1, 0);
+    memset(visited, 0, sizeof(visited));
+    max_len = 0;
+    dfs(max_node, 0);
+    printf("%d\n", max_len);
+    return 0;
 
 }
