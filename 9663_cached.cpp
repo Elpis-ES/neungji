a2e255f6a88e865d23f3b1af7f8b4cf90b4efa34
Elpis <elpis@Elpisui-MacBookAir.local>
2022-02-19 11:41:18 +0900

    Delete source on main directory

--- 9663_cached.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-
-int queen[14] = {1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 14200, 73712, 365596};
-
-int main(){
-    int n;
-    scanf(" %d", &n);
-    printf("%d\n", queen[n-1]);
-}
