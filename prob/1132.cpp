1bf1c56f70b6fb0fe6a2d571c2a0b14e44ba33e9
Elpis <sukkie95@gmail.com>
2022-04-12 00:16:35 +0900

    1132: Greedy

--- prob/1132.cpp
+++ prob/1132.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#define ll long long
 
 using namespace std;
 
@@ -7,11 +8,20 @@ int main(){
     cin.tie(nullptr);
 
     vector<string> s;
-    vector<pair<int, int>> v(10, make_pair(-1, false));
+    vector<ll> v(10, 0);
+    vector<bool> can_be_zero(10, true);
 
     int n;
     cin >> n;
 
+    vector<ll> pow10;
+    ll p = 1;
+    while(pow10.size() != 14){
+        pow10.emplace_back(p);
+        p *= 10;
+    }
+
+    int alpha_used = 0;
     for(int i = 0; i < n; i++){
         string ss;
         cin >> ss;
@@ -19,15 +29,33 @@ int main(){
         for(int j = 0; j < ss.length(); j++){
             int target_idx = ss[j] - 'A';
             int cur_digit = ss.length() - j - 1;
-            v[target_idx].first = max(v[target_idx].first, cur_digit);
-            if(cur_digit == 0){
-                v[target_idx].second = 1;
+            if(v[target_idx] == 0){
+                alpha_used++;
+            }
+            v[target_idx] += pow10[cur_digit];
+            if(j == 0){
+                can_be_zero[target_idx] = false;
             }
         }
     }
-    for(auto [p, q] : v){
-        cout << "(" << p << ", " << q << ") ";
-    }
 
-    cout << "\n";
+    if(alpha_used == 10){
+        ll cur_min = LLONG_MAX;
+        int min_idx = -1;
+        for(int i = 0; i < 10; i++){
+            if(can_be_zero[i] && cur_min > v[i]){
+                cur_min = v[i];
+                min_idx = i;
+            }
+        }
+        v[min_idx] = 0;
+    }
+    sort(v.begin(), v.end());
+
+    ll sum = 0;
+    for(ll i = 0; i < 10; i++){
+        sum += i * v[i];
+    }
+    cout << sum << "\n";
+
 }
