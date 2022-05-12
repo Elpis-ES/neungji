720b2df933223e895c09434f9141123d36cf4920
Elpis <sccho@rtst.co.kr>
2022-05-12 12:26:24 +0900

    9252: Remove useless string operation

--- prob/9252.cpp
+++ prob/9252.cpp
@@ -4,16 +4,7 @@ using namespace std;
 string a, b;
 int a_len, b_len;
 vector<vector<int>> dp_arr;
-vector<vector<string>> dp_str;
-
-string get_str(int p, int q){
-    if(p < 0 || q < 0){
-        return "";
-    }
-    else{
-        return dp_str[p][q];
-    }
-}
+vector<vector<pair<int, int>>> prev_vec;
 
 int dp(int p, int q){
     if(p < 0 || q < 0){
@@ -25,18 +16,27 @@ int dp(int p, int q){
     int cur;
     if(a[p] == b[q]){
         cur = dp(p - 1, q - 1) + 1;
-        dp_str[p][q] = get_str(p - 1, q - 1) + a[p];
+        if(p - 1 >= 0 && q - 1 >= 0){
+            prev_vec[p][q].first = p - 1;
+            prev_vec[p][q].second = q - 1;
+        }
     }
     else{
         int l = dp(p - 1, q);
         int m = dp(p, q - 1);
         if(l > m){
             cur = l;
-            dp_str[p][q] = get_str(p - 1, q);
+            if(p - 1 >= 0){
+                prev_vec[p][q].first = p - 1;
+                prev_vec[p][q].second = q;
+            }
         }
         else{
             cur = m;
-            dp_str[p][q] = get_str(p, q - 1);
+            if(q - 1 >= 0){
+                prev_vec[p][q].first = p;
+                prev_vec[p][q].second = q - 1;
+            }
         }
     }
     dp_arr[p][q] = cur;
@@ -53,8 +53,27 @@ int main(){
     b_len = b.length();
 
     dp_arr = vector<vector<int>> (a_len, vector<int> (b_len, -1));
-    dp_str = vector<vector<string>> (a_len, vector<string> (b_len, ""));
+    prev_vec = vector<vector<pair<int, int>>> (a_len, vector<pair<int, int>> (b_len, make_pair(-1, -1)));
 
     cout << dp(a_len - 1, b_len - 1) << "\n";
-    cout << get_str(a_len - 1, b_len - 1) << "\n";
+
+    vector<char> c;
+
+    int x = a_len - 1;
+    int y = b_len - 1;
+    while(!(x == -1 && y == -1)){
+        if(a[x] == b[y]){
+            c.emplace_back(a[x]);
+        }
+        int tmp_x = prev_vec[x][y].first;
+        int tmp_y = prev_vec[x][y].second;
+        x = tmp_x;
+        y = tmp_y;
+    }
+
+    for(auto it = c.rbegin(); it != c.rend(); it++){
+        cout << *it;
+    }
+    cout << "\n";
+
 }
