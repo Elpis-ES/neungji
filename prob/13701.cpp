6cc19b6a7914ec5927d5644d1c1539c73e3ecdc2
Elpis <sccho@rtst.co.kr>
2022-05-17 12:43:29 +0900

    13701: Bitmasking

--- prob/13701.cpp
+++ prob/13701.cpp
@@ -3,18 +3,16 @@
 using namespace std;
 
 int main(){
-    string s;
-    getline(cin, s);
-    s.push_back(' ');
-    unordered_set <int> sss;
-    string separate = " ";
-    size_t pos = 0;
-    while((pos = s.find(separate)) != string::npos){
-        sss.insert( atoi(s.substr(0, pos).c_str()));
-        s.erase(0, pos+separate.length());
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    vector<bool> v ((1 << 25), false);
+    int i;
+    while(cin >> i){
+        if(v[i] == false){
+            cout << i << " ";
+            v[i] = true;
+        }
     }
-    for(auto it = sss.begin(); it!=sss.end(); it++){
-        printf("%d ", *it);
-    }
-    printf("\n");
+    cout << "\n";
+
 }
